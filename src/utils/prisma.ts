27daa26615941758033a4866cd843a77c529d706
WilliamKelly00 <williammkelly00@gmail.com>
2022-06-22 23:44:05 -0400

    added prisma and endpoint for voting

---
+++ src/utils/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global{
+    var prisma : PrismaClient;
+}
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
